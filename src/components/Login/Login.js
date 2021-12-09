58ce6e56bc8789b4e0d61c05bd39ffd15c68de34
mgenovski <mgenovski@gmail.com>
2021-12-09 21:41:14 +0200

    Fix Login input alignment

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,10 +5,11 @@ const Login = () => {
         <div className="login">
             <div className='login-wrapper'>
                 <h1>Login</h1>
+                <div>
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email" name="email" placeholder='peter@abv.bg'></input>
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="passwors" placeholder='123456'></input>
+                <input type="text" id="email" name="email" placeholder='peter@abv.bg'></input></div>
+                <div><label htmlFor="password">Password</label>
+                <input type="password" id="password" name="passwors" placeholder='123456'></input></div>
                 <button className='normal'>Login</button>
             </div>
         </div>
