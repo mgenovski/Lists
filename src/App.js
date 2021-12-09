045911206a6344a1f1d39736388b04d59f169ae6
mgenovski <mgenovski@gmail.com>
2021-12-09 19:07:48 +0200

    add main layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+        <div className="logo">
+            <a href="/">LISTS</a>
+            A SoftUni React Project
+        </div>
+        <nav>
+            <a href="/">All lists</a>
+            <a href="/">Login</a>
+            <a href="/">Register</a>
+        </nav>
+    </header>
+    <div className="content">
+        <div className="welcome">
+            <div className="welcome-title">
+                <h1>Hi. This is Lists.</h1>
+                <p>Create, share and use different checklists.</p>
+            </div>
+            <div className="welcome-buttons">
+                <a href="/">View all lists</a>
+            </div>
+        </div>
+    </div>
+    <footer>© Lists. All rights reserved!</footer>
     </div>
   );
 }
