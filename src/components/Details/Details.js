5d9722eb7c6254f6bf1bc56d38fa1a02d23eebc2
mgenovski <mgenovski@gmail.com>
2021-12-10 22:37:19 +0200

    Add remove list item

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -5,10 +5,12 @@ import * as listService from '../../services/listService.js';
 import { useAuthContext } from '../../contexts/AuthContext';
 
 import './Details.css';
-const Item = ({ item, index }) => {
+const Item = ({ item, index, userId, onItemRemove }) => {
+    const { user } = useAuthContext();
     return (
         <div>
             <span>{item.text}</span>
+            {user._id === userId ? <button className="del" onClick={() => onItemRemove(index)}>✕</button> : ''}
         </div>
     )
 }
@@ -33,7 +35,8 @@ const Details = () => {
 
     const onItemAdd = e => {
         e.preventDefault();
-        let formData = new FormData(e.currentTarget);
+        const form = e.currentTarget;
+        let formData = new FormData(form);
         const item = formData.get('item');
         if (item === '') return;
         const newItems = [...list.items, { text: item, isDone: false }];
@@ -54,20 +57,48 @@ const Details = () => {
         listService.update(listInfo, user.accessToken)
             .then(result => {
                 setList(listInfo);
+                form.reset();
                 // navigate(`/details/${list._id}`);
             })
     };
+
+    const onItemRemove = (index) => {
+        const newItems = [...list.items];
+        newItems.splice(index, 1);
+        const listInfo = {
+            title: list.title,
+            description: list.description,
+            category: list.category,
+            type: list.type,
+            shared: list.shared,
+            items: newItems,
+            _userId: list._userId,
+            _ownerName: list._ownerName,
+            _ownerId: list._ownerId,
+            _id: list._id
+        }
+
+        listService.update(listInfo, user.accessToken)
+            .then(result => {
+                setList(listInfo);
+                // navigate(`/details/${list._id}`);
+            })
+    };
+
+    const addForm = (
+        <form onSubmit={onItemAdd}>
+            <div><input type="text" id="item" name="item" placeholder="Item..." /></div>
+            <div><button className='small'>Add item</button></div>
+        </form>
+    )
 
     return (
         <>
             <div className='details'>
                 <div className='list'>
                     <h2>{list.title}</h2>
-                    {list.items ? list.items.map((item, index) => (<Item key={index} item={item} />)) : null}
-                    {user._id === list._userId ? (<form onSubmit={onItemAdd}>
-                        <div><input type="text" id="item" name="item" placeholder="Item..." /></div>
-                        <div><button className='small'>Add</button></div>
-                    </form>) : ''}
+                    {list.items ? list.items.map((item, index) => (<Item key={index} userId={list._userId} item={item} index={index} onItemRemove={onItemRemove} />)) : null}
+                    {user._id === list._userId ? addForm : ''}
                 </div>
                 <div className='info'>
                     <h2>Information</h2>
